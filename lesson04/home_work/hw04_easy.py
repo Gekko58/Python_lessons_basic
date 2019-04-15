a9c1ec0e25d2b2ad1a9663ead4ac6eebaad0767b
Gekko <evgeni.zhukov@gmail.com>
2019-04-15 08:01:06 +0300

    Выполнил второе задание easy

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -12,13 +12,20 @@ def first_task():
     input_list = [random.randint(1, 20) for _ in range(10)] #Генерируем произвольный список
     print('Исходный список: ', input_list)
     output_list = [(index ** 2) for index in input_list]
-
-
     print(output_list)
+    return None
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+def second_task():
+    source_list = ['яблоко', 'груша', 'апельсин', 'слива', 'персик', 'абрикос', 'дыня', 'арбуз', 'лимон', 'мандарин']
+    first_list = [source_list[random.randint(0, len(source_list) - 1)] for _ in range(5)]
+    second_list = [source_list[random.randint(0, len(source_list) - 1)] for _ in range(5)]
+    print(f"Первый список: {first_list}\nВторой список: {second_list}")
+    output_list = set(first_list).union(set(second_list))
+    print(f"Общий список фруктов: {output_list}")
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -33,6 +40,8 @@ try:
     user_input = int(user_input)
     if user_input == 1:
         first_task()
+    elif user_input == 2:
+        second_task()
     else:
         print('Не верный ввод')
 except ValueError:
